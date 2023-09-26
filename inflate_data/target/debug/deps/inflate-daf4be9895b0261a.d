35841e26db054bb8c6c1870de54e7b165cad34fd
bishop <bishop@example.com>
2023-09-26 18:36:19 +0200

    fix(remove debug) : remove debug version

--- inflate_data/target/debug/deps/inflate-daf4be9895b0261a.d
+++
@@ -1,9 +0,0 @@
-C:\Users\louis\Desktop\rs-project\rs_grabber\inflate_data\target\debug\deps\inflate-daf4be9895b0261a.rmeta: C:\Users\louis\.cargo\registry\src\index.crates.io-6f17d22bba15001f\inflate-0.4.5\src\lib.rs C:\Users\louis\.cargo\registry\src\index.crates.io-6f17d22bba15001f\inflate-0.4.5\src\checksum.rs C:\Users\louis\.cargo\registry\src\index.crates.io-6f17d22bba15001f\inflate-0.4.5\src\writer.rs C:\Users\louis\.cargo\registry\src\index.crates.io-6f17d22bba15001f\inflate-0.4.5\src\utils.rs C:\Users\louis\.cargo\registry\src\index.crates.io-6f17d22bba15001f\inflate-0.4.5\src\reader.rs
-
-C:\Users\louis\Desktop\rs-project\rs_grabber\inflate_data\target\debug\deps\inflate-daf4be9895b0261a.d: C:\Users\louis\.cargo\registry\src\index.crates.io-6f17d22bba15001f\inflate-0.4.5\src\lib.rs C:\Users\louis\.cargo\registry\src\index.crates.io-6f17d22bba15001f\inflate-0.4.5\src\checksum.rs C:\Users\louis\.cargo\registry\src\index.crates.io-6f17d22bba15001f\inflate-0.4.5\src\writer.rs C:\Users\louis\.cargo\registry\src\index.crates.io-6f17d22bba15001f\inflate-0.4.5\src\utils.rs C:\Users\louis\.cargo\registry\src\index.crates.io-6f17d22bba15001f\inflate-0.4.5\src\reader.rs
-
-C:\Users\louis\.cargo\registry\src\index.crates.io-6f17d22bba15001f\inflate-0.4.5\src\lib.rs:
-C:\Users\louis\.cargo\registry\src\index.crates.io-6f17d22bba15001f\inflate-0.4.5\src\checksum.rs:
-C:\Users\louis\.cargo\registry\src\index.crates.io-6f17d22bba15001f\inflate-0.4.5\src\writer.rs:
-C:\Users\louis\.cargo\registry\src\index.crates.io-6f17d22bba15001f\inflate-0.4.5\src\utils.rs:
-C:\Users\louis\.cargo\registry\src\index.crates.io-6f17d22bba15001f\inflate-0.4.5\src\reader.rs:
--- inflate_data/target/debug/deps/inflate_data-40ed652127d661eb.d
+++
@@ -1,5 +0,0 @@
-C:\Users\louis\Desktop\rs-project\rs_grabber\inflate_data\target\debug\deps\inflate_data-40ed652127d661eb.rmeta: src\main.rs
-
-C:\Users\louis\Desktop\rs-project\rs_grabber\inflate_data\target\debug\deps\inflate_data-40ed652127d661eb.d: src\main.rs
-
-src\main.rs:
